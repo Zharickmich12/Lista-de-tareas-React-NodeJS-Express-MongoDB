16c48cf559bd4bf23000f39fae5ef8d78a831ea8
Zharick Michelle <128750315+Zharickmich12@users.noreply.github.com>
2023-08-22 18:39:20 -0500

    review-3

--- my-react-task-list/src/App.jsx
+++ my-react-task-list/src/App.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Input, Button, Spacer } from '@chakra-ui/react';
+import { Box, Flex, Input, Button, ChakraProvider, ColorModeScript, extendTheme, useColorMode } from '@chakra-ui/react';
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Header } from './Components/Header';
@@ -7,6 +7,39 @@ import { useTaskManager } from './useTaskManager';
 import Home from './Components/Home';
 import SobreNosotros from './Components/SobreNosotros';
 import Menu from './Components/Menu';
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark', 
+    useSystemColorMode: false,
+  },
+  colors: {
+    rosybrown: {
+      50: '#f6e7e7',
+    },
+    mistyrose: {
+      50: '#fdf2f2',
+    },
+    gray: {
+      50: '#ffffff',
+    },
+    black: '#000000',
+    white: '#ffffff',
+  },
+  fonts: {
+    heading: 'Arial, sans-serif',
+    body: 'Arial, sans-serif',
+  },
+  styles: {
+    global: (props) => ({
+      body: {
+        fontFamily: 'body',
+        color: props.colorMode === 'dark' ? 'gray.100' : 'gray.800',
+        bg: props.colorMode === 'dark' ? 'gray.900' : 'white',
+      },
+    }),
+  },
+});
 
 const App = () => {
   const { tasks, createTask, deleteTask, toggleComplete } = useTaskManager();
@@ -25,48 +58,51 @@ const App = () => {
   };
 
   return (
+    <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <Router>
       <Box bg="black" color="white" minHeight="100vh">
         <Header />
-        <Menu />
-        <Flex justify="center" align="center" mt={4}>
-          <Box w="800px" p={4}>
-            <Input
-              placeholder="Nueva tarea"
-              value={newTaskName}
-              onChange={(e) => setNewTaskName(e.target.value)}
-              color="rosybrown"
-              _placeholder={{ color: 'rosybrown' }}
-              mb={2}
-            />
-            <Input
-              placeholder="Descripción (opcional)"
-              value={newTaskDescription}
-              onChange={(e) => setNewTaskDescription(e.target.value)}
-              color="rosybrown"
-              _placeholder={{ color: 'rosybrown' }}
-              mb={2}
-            />
-            <Button
-              colorScheme="rosybrown"
-              bg="rosybrown" 
-              onClick={handleAddTask}
-              mb={2}
-            >
-              Añadir tarea
-            </Button>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route
-                path="/tareas"
-                element={<Tasklist tasks={tasks} onDelete={deleteTask} onToggleComplete={toggleComplete} />}
+          <Menu />
+          <Flex justify="center" align="center" mt={4}>
+            <Box w="800px" p={4}>
+              <Input
+                placeholder="Nueva tarea"
+                value={newTaskName}
+                onChange={(e) => setNewTaskName(e.target.value)}
+                color="rosybrown"
+                _placeholder={{ color: 'rosybrown' }}
+                mb={2}
               />
-              <Route path="/sobre-nosotros" element={<SobreNosotros />} />
-            </Routes>
-          </Box>
-        </Flex>
-      </Box>
-    </Router>
+              <Input
+                placeholder="Descripción (opcional)"
+                value={newTaskDescription}
+                onChange={(e) => setNewTaskDescription(e.target.value)}
+                color="rosybrown"
+                _placeholder={{ color: 'rosybrown' }}
+                mb={2}
+              />
+              <Button
+                colorScheme="rosybrown"
+                bg="rosybrown"
+                onClick={handleAddTask}
+                mb={2}
+              >
+                Añadir tarea
+              </Button>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route
+                  path="/tareas"
+                  element={<Tasklist tasks={tasks} onDelete={deleteTask} onToggleComplete={toggleComplete} />}
+                />
+                <Route path="/sobre-nosotros" element={<SobreNosotros />} />
+              </Routes>
+            </Box>
+          </Flex>
+        </Box>
+      </Router>
+    </ChakraProvider>
   );
 };
 
--- my-react-task-list/src/Components/Header.jsx
+++ my-react-task-list/src/Components/Header.jsx
@@ -1,11 +1,16 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Button, useColorMode } from '@chakra-ui/react';
 
 export const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Box bg="gray.900" p={4} mb={4}>
-  <Heading as="h1" size="xl" color="rosybrown" textAlign="center">
-    To-do List
-  </Heading>
-</Box>
+    <Box bg="gray.900" p={4} mb={4} display="flex" justifyContent="space-between">
+      <Heading as="h1" size="xl" color="rosybrown" textAlign="center">
+        To-do List
+      </Heading>
+      <Button onClick={toggleColorMode}>
+        {colorMode === 'light' ? 'Modo Oscuro' : 'Modo Claro'}
+      </Button>
+    </Box>
   );
 };
